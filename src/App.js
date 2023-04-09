07dd44c95bbc740ba1f102e50861dfa7e48ea323
Akhil Mohan M J <inboxakhil321@gmail.com>
2023-04-09 18:37:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Candycrush from "./components/candycrush/candycrush";
+import Auth from "./layout/Auth";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+     <Candycrush/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/candycrush/Candycrush.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Candycrush() {
+  return (
+    <div>candycrush</div>
+  )
+}
