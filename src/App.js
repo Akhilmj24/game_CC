4852ed8662df29a6b45d3261a4efceb25c65f02c
Akhil Mohan M J <inboxakhil321@gmail.com>
2023-04-09 18:38:39 +0530

    err fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Candycrush from "./components/candycrush/candycrush";
-import Auth from "./layout/Auth";
-import Layout from "./layout/Layout";
+import Candycrush from "./components/candycrush/Candycrush";
+
 
 function App() {
   return (
